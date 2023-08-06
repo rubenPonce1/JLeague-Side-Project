6fdf13b1075ffef76840998469bf45d451b05167
Ruben Ponce <rponce3@mail.sfsu.edu>
2023-08-05 19:17:12 -0700

    Some Navbar changes
    Tweaked the Navbar a little, dropdown not responding.
    TO DO:
    -Need to fix dropdown for more

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,56 +5,47 @@ Authors: Ruben Ponce
 Description: File for navigation bar.
 */
 
-
 import React, {useState} from "react";
-import { useRef } from "react";
+import { Link} from 'react-router-dom';
+
 import Dropdown from './Dropdown'
-
 import "../Navbar/Navbar.css";
-import { Link, useNavigate, generatePath, Navigate} from 'react-router-dom';
-
 import J1Logo from '../images/JLeague_Logo/Jleague(3).png'
 
 
 function Navbar(){
+
     const [click, setClick] = useState(false)
     const [dropdown, setDropdown] = useState(false);
 
     const handleClick = () => setClick(!click);
     const closeMobileMenu = () => setClick(false);
 
-    const onMouseEnter = () =>{
-      if (window.innerWidth < 960){
-        setDropdown(true);
-      }
-      else{
-        setDropdown(true);
-      }
+    const onMouseEnter = () => {
+      setDropdown(true)
     };
 
-    const onMouseLeave = () =>{
-      if (window.innerWidth < 960){
-        setDropdown(false);
-      }
-      else{
-        setDropdown(false);
-      }
+    const onMouseLeave = () => {
+      setDropdown(false)
     };
 
 
     return(
       <>
         <nav className="navbar">
-          <Link to='/' className="navbar-logo">
+          <Link to='/' className="navbar-logo" onClick={closeMobileMenu}>
             <img src={J1Logo} className ="J1-logo"></img>  
           </Link>
+
           <div className="menu-icon" onClick={handleClick}>
             <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
           </div>
           <ul className={click ? 'nav-menu active' : 'nav-menu'}>
 
             <li className='nav-item'>
-              <Link to='/' className='nav-links' onClick={closeMobileMenu}> Fixtures </Link>
+              <Link to='/Fixtures' className='nav-links' onClick={closeMobileMenu}> 
+              Fixtures
+              </Link>
             </li>
 
             <li className='nav-item'>
@@ -68,16 +59,16 @@ function Navbar(){
                           </Link>
             </li>
 
-            <li 
+            <li
               className='nav-item'
-              onMouseEnter={onMouseEnter}
+              onMouseEnter={onMouseEnter} 
               onMouseLeave={onMouseLeave}
             >
 
-              <Link to='/Services' className='nav-links' onClick={closeMobileMenu}>
+              <Link to='/More' className='nav-links' onClick={closeMobileMenu}>
                 More <i className='fas fa-caret-down' />
               </Link>
-              {dropdown && <Dropdown/>}
+              {dropdown && <Dropdown />}
             </li>
 
             
