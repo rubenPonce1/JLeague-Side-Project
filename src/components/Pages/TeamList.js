40f7f3bd945a8ad1c611faa93453a0cc5c61cf11
Ruben Ponce <rponce3@mail.sfsu.edu>
2023-08-13 17:00:47 -0700

    Create TeamList.js
    Created an array list of all the teams with their logos, and the path to their pages that  will be created later

---
+++ src/components/Pages/TeamList.js
@@ -0,0 +1,155 @@
+/* 
+Filename: TeamList.js
+Date: 08/08/23
+Authors: Ruben Ponce
+Description: File of array of all the Teams Name, Logo, and page
+*/
+import Albirex from "../images/Team_Logos/Albirex_Niigata.png"
+import Avispa from "../images/Team_Logos/Avispa_Fukuoka.png"
+import Cerezo from "../images/Team_Logos/Cerezo_Osaka.png"
+import FCTokyo from "../images/Team_Logos/FC_Tokyo.png"
+import Gamba from "../images/Team_Logos/Gamba_Osaka.png"
+import Hokkaido from "../images/Team_Logos/Hokkaido_Consadole_Sapporo.png"
+import Kashima from "../images/Team_Logos/Kashima_Antlers.png"
+import Kashiwa from "../images/Team_Logos/Kashiwa_Reysol.png"
+import Kawasaki from "../images/Team_Logos/Kawasaki_Frontale.png"
+import Kyoto from "../images/Team_Logos/Kyoto_Sanga_FC.png"
+import Nagoya from "../images/Team_Logos/Nagoya_Grampus.png"
+import Sagan from "../images/Team_Logos/Sagan_Tosu.png"
+import Sanfrecce from "../images/Team_Logos/Sanfrecce_Hiroshima.png"
+import Shonan from "../images/Team_Logos/Shonan_Bellmare.png"
+import Urawa from "../images/Team_Logos/Urawa_Red_Diamonds.png"
+import Vissel from "../images/Team_Logos/Vissel_Kobe.png"
+import YokohamaFMarinos from "../images/Team_Logos/Yokohama_F_Marinos.png"
+import YokohamaFC from "../images/Team_Logos/Yokohama_FC.png"
+
+export const TeamList =[
+    {
+        image: Albirex,
+        title: 'Albirex',
+        path: '/Albirex' ,
+        cName: 'team-link'
+    },
+
+    {
+        image: Avispa,
+        title: 'Avispa',
+        path: '/Avispa' ,
+        cName: 'team-link'
+    },
+
+    {
+        image: Cerezo,
+        title: 'Cerezo Osaka',
+        path: '/Cerezo_Osaka' ,
+        cName: 'team-link'
+    },
+
+    {
+        image: FCTokyo,
+        title: 'FC Tokyo',
+        path: '/FC_Tokyo' ,
+        cName: 'team-link'
+    },
+
+    {
+        image: Gamba,
+        title: 'Gamba Osaka',
+        path: '/Gamba_Osaka' ,
+        cName: 'team-link'
+    },
+
+    {
+        image: Hokkaido,
+        title: 'Hokkaido Consadole Sapporo',
+        path: '/Hokkaido' ,
+        cName: 'team-link'
+    },
+
+    {
+        image: Kashima,
+        title: 'Kashima Antlers',
+        path: '/Kashima_Antlers' ,
+        cName: 'team-link'
+    },
+
+    {
+        image: Kashiwa,
+        title: 'Kashiwa Reysol',
+        path: '/Kashiwa_Reysol' ,
+        cName: 'team-link'
+    },
+
+    {
+        image: Kawasaki,
+        title: 'Kawasaki Frontale',
+        path: '/Kawasaki_Frontale' ,
+        cName: 'team-link'
+    },
+
+    {
+        image: Kyoto,
+        title: 'Kyoto Sanga FC',
+        path: '/Kyoto_Sanga_FC' ,
+        cName: 'team-link'
+    },
+
+    {
+        image: Nagoya,
+        title: 'Nagoya Grampus',
+        path: '/Nagoya_Grampus' ,
+        cName: 'team-link'
+    },
+
+    {
+        image: Sagan,
+        title: 'Sagan Tosu',
+        path: '/Sagan_Tosu' ,
+        cName: 'team-link'
+    },
+
+    {
+        image: Sanfrecce,
+        title: 'Sanfrecce Hiroshima',
+        path: '/Sanfreece Hiroshima' ,
+        cName: 'team-link'
+    },
+
+    {
+        image: Shonan,
+        title: 'Shonan Bellmare',
+        path: '/Shonan_Bellmare' ,
+        cName: 'team-link'
+    },
+
+    {
+        image: Urawa,
+        title: 'Urawa Red Diamonds',
+        path: '/Urawa_Reds' ,
+        cName: 'team-link'
+    },
+
+    {
+        image: Vissel,
+        title: 'Vissel Kobe',
+        path: '/Vissel_Kobe' ,
+        cName: 'team-link'
+    },
+
+    {
+        image: YokohamaFMarinos,
+        title: 'Yokohama F Marinos',
+        path: '/Yokohama_F_Marinos' ,
+        cName: 'team-link'
+    },
+
+    {
+        image: YokohamaFC,
+        title: 'Yokohama FC',
+        path: '/Yokohama_FC' ,
+        cName: 'team-link'
+    },
+
+    
+
+]
