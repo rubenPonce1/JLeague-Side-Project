04b2ee7947faef30cc8b48043d9a09fe0287401b
Ruben Ponce <rponce3@mail.sfsu.edu>
2023-08-13 21:47:16 -0700

    Update Home.js
    An updated Home page that has all the teams with their logo, team name, and clickable link to them.

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -1,5 +1,6 @@
-import React from 'react';
-
+import React, {useState} from 'react';
+import { TeamList } from './TeamList';
+import { Link } from 'react-router-dom';
 import '../Styles/Home.css';
 
 
@@ -7,18 +8,43 @@ import 'bootstrap/dist/css/bootstrap.css';
 import Container from 'react-bootstrap/Container';
 
 export default function Home(){
+    const [click, setClick] = useState(false);
+    const handleClick = () => setClick(!click);
+
     return(
     <div>
-      <h1>
-        Jleague
+      <h1 className='HeaderName'>
+        J.LEAGUE
       </h1>
+
+
       {/*This Container is for the Youtube Video */}
-      <Container>
+      <Container className='videoContainer'>
         <div class="ratio ratio-16x9">
-          <iframe src="https://www.youtube.com/embed/Mu3H6-SbGGQ" title="YouTube video" allowFullScreen></iframe>
+          <iframe src="https://www.youtube.com/embed/Mu3H6-SbGGQ" 
+                  title="YouTube video" allowFullScreen></iframe>
         </div>  
       </Container>
 
+
+      {/* This is the list of all the J1 Teams*/}
+      <>
+        <ul className='team-list'>
+          {TeamList.map((item, index) => {
+            return(
+              <li key={index} >
+                <Link className={item.cName}
+                      to = {item.path}>
+
+                    <img src= {item.image} className= "team-logo"></img>
+                    {item.title}  
+                    
+                </Link>   
+              </li>
+            )
+          })}
+        </ul>
+      </>
     </div>
     );
        
