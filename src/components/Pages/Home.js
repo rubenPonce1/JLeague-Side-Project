2162f7b75d88cb4ce9f1f599dcfa00796a4e00d9
Ruben Ponce <rponce3@mail.sfsu.edu>
2023-08-22 21:34:29 -0700

    Update Home.js
    Added Carousel to the home page and added some comments to understand it better.

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -1,4 +1,13 @@
-import React, {useState} from 'react';
+/* 
+Filename: Home.js
+Date: 08/22/23
+Authors: Ruben Ponce
+Description: File for the home page of the webpage
+it shows a Youtube video, has a image carousel, and team pages
+*/
+
+
+import React from 'react';
 import { TeamList } from './TeamList';
 import { Link } from 'react-router-dom';
 import '../Styles/Home.css';
@@ -6,12 +15,13 @@ import '../Styles/Home.css';
 
 import 'bootstrap/dist/css/bootstrap.css';
 import Container from 'react-bootstrap/Container';
+import Carousel from '../Carousel/Carousel';
+import { homeSlides } from '../Carousel/ImgSlideshow';
+
+
 
 export default function Home(){
-    const [click, setClick] = useState(false);
-    const handleClick = () => setClick(!click);
-
-    return(
+  return(
     <div>
       <div className='home'>
         <h1 className='HeaderName'>
@@ -20,33 +30,42 @@ export default function Home(){
 
 
         {/*This Container is for the Youtube Video */}
-        <Container className='videoContainer'>
+        <Container className='video-cntr'>
           <div class="ratio ratio-16x9">
             <iframe src="https://www.youtube.com/embed/Mu3H6-SbGGQ" 
                     title="YouTube video" allowFullScreen></iframe>
           </div>  
         </Container>
 
-      
-      {/* This is the list of all the J1 Teams*/}
-      <>
-        <ul className='team-list'>
-          {TeamList.map((item, index) => {
-            return(
-              <li key={index} >
-                <Link className={item.cName}
-                      to = {item.path}>
 
-                    <img src= {item.image} className= "team-logo"></img>
-                    {item.title}  
-                    
-                </Link>   
-              </li>
-            )
-          })}
-        </ul>
-      </>
-      </div>
+        {/* This is an image slideshow */}
+        <div className='home-carousel'>
+          <Carousel images={homeSlides}/>
+        </div>
+        
+
+        
+        {/* This is the list of all the J1 Teams*/}
+        <div className='team-list-cntr'>
+        <h2 className='team-list-hdr'> TEAM LIST</h2>
+          <ul className='team-list'>
+            {TeamList.map((item, index) => {
+              return(
+                <li key={TeamList.index} >
+                  <Link className={item.cName}
+                        to = {item.path}>
+
+                      <img src= {item.image} className= "team-logo" alt='teamLogo'></img>
+                      {item.title}  
+                      
+                  </Link>   
+                </li>
+              )
+            })}
+          </ul>
+        </div>
+      
+    </div>
     </div>
     );
        
