06180d1e9b476f833f8a4b53b1696fc5eba8cb4e
Ruben Ponce <rponce3@mail.sfsu.edu>
2023-09-19 20:50:13 -0700

    added cors
    added cors to the backend so that it can be run in the frontend

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import mysql2 from "mysql2"
+import cors from "cors";
 
 
 
@@ -17,6 +18,7 @@ const db = mysql2.createConnection({
 });
 
 app.use(express.json())
+app.use(cors())
 
 app.get("/", (req,res)=>{
     res.json("hello this is the backend")
