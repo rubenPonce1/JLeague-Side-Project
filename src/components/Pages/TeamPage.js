34627a5a5221b3bc58f9e06cdd793424ca9fb893
Ruben Ponce <rponce3@mail.sfsu.edu>
2023-09-19 21:10:37 -0700

    Update TeamPage.js
    Skeleton of the SQL Workbench team page

--- src/components/Pages/TeamPage.js
+++ src/components/Pages/TeamPage.js
@@ -8,7 +8,7 @@ const TeamPage = () => {
     const fetchAllteams = async () =>{
       try{
         const res = await axios.get("http://localhost:8800/teams");
-        console.log(res);
+        setTeams(res.data)
       }catch(err){
         console.log(err);
       }
@@ -16,7 +16,21 @@ const TeamPage = () => {
     fetchAllteams();
   }, []);
 
-  return <div>TeamPage</div>;
+  return <div>
+    <h1>Testing Team Page </h1>
+    <div className="teams">
+      {teams.map((team)=>(
+        <div className="team" key={team.id}>
+          {team.teamImg && <img src={team.teamImg} alt = ""/>}
+          <h2>{team.TeamName}</h2>
+          <p>{team.teamInfo}</p>
+
+        </div>
+      ))};
+      
+
+    </div>
+  </div>;
 };
 
 export default TeamPage;
