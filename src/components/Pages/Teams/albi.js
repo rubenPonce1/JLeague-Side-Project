082a7c24327faaa58dee91776bb5344a7f84c721
Ruben Ponce <rponce3@mail.sfsu.edu>
2023-11-14 14:02:28 -0800

    Create albi.js
    This is the team page for Albirerx, this will be the test page for all the other teams.

---
+++ src/components/Pages/Teams/albi.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import '../Styles/TeamPage.css'
+
+export default function Albirex(){
+    return(
+        <div></div>
+    );
+}   
