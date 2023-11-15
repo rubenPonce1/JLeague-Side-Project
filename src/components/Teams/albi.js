e56b9ab71341c04ef5859f04918325a25cbb646c
Ruben Ponce <rponce3@mail.sfsu.edu>
2023-11-14 21:32:42 -0800

    Update albi.js
    added a youtube video to the team page

--- src/components/Teams/albi.js
+++ src/components/Teams/albi.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Container from 'react-bootstrap/esm/Container';
 import {Link} from 'react-router-dom';
 
 import '../Styles/TeamPage.css';
@@ -6,8 +7,16 @@ import '../Styles/TeamPage.css';
 
 export default function Albirex(){
     return(
+      <div>
         <h1 className='teamName'> Albirex </h1>
 
-        
+        <Container className='video-cntr'>
+          <div class="ratio ratio-16x9">
+            <iframe src="https://www.youtube.com/embed/O8PXLauk_V8?si=6Z4gHMr4UXJ4aYXN" 
+                    title="YouTube video" allowFullScreen></iframe>
+          </div> 
+        </Container>
+      </div>
+
     );
 }   
