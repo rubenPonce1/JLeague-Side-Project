56a0a30b5c5b9685a1e4c4540bc83b6299d26c01
Ruben Ponce <rponce3@mail.sfsu.edu>
2023-11-16 15:03:00 -0800

    Update albi.js
    some changes to the naming convention and location of certain elements

--- src/components/Teams/albi.js
+++ src/components/Teams/albi.js
@@ -8,36 +8,39 @@ import Albirex from "../images/Team_Logos/Albirex_Niigata.png"
 export default function Albi(){
     return(
       <div className='albi'>
-        <h1 className='header'> Albirex Niigata </h1>
-        <img src={Albirex} alt = "logo"className ="team-logo"></img> 
+        <h1 className='header'> <img src={Albirex} alt = "logo"className ="team-logo"></img> 
+        Albirex Niigata 
+        </h1>
+        
 
         <div className='team-info'> 
-          <div className='info-title'> Info </div>
+          <div className='team-info-wrapper'>
+            <div className='info-title'> Info </div>
 
-          <div className='title'> EST: </div> 
-            <div className='value'>1955</div>
+            <div className='title'> EST: </div> 
+              <div className='value'>1955</div>
 
-          <div className='title'>Team names: </div>
-            <div className='value'>
-              Niigata Eleven SC (1955),
-              Albireo Niigata FC (1995),
-              Albirex Niigata (1997),
-              Nickname: Albi
-            </div> 
-          
-            <div className='title'> Stadium: </div>
-              <div className='value'> 
-                Denka Big Swan Stadium<br/>
-                Capacity: 42,300<br/>
-                <i class="fas fa-map-marker-alt"> </i> Chuo-ku, Niigata<br/>
-              </div>
-              
-            <div className='title'>Accomplishments: </div>
+            <div className='title'>Team names: </div>
               <div className='value'>
-                J2 Champion (2003, 2022)
-              </div>
+                Niigata Eleven SC (1955),
+                Albireo Niigata FC (1995),
+                Albirex Niigata (1997),
+                Nickname: Albi
+              </div> 
+            
+              <div className='title'> Stadium: </div>
+                <div className='value'> 
+                  Denka Big Swan Stadium<br/>
+                  Capacity: 42,300<br/>
+                  <i class="fas fa-map-marker-alt"> </i> Chuo-ku, Niigata<br/>
+                </div>
+                
+              <div className='title'>Accomplishments: </div>
+                <div className='value'>
+                  J2 Champion (2003, 2022)
+                </div>
           
-          
+          </div>
         </div>
 
         <Container className='video-cntr'>
