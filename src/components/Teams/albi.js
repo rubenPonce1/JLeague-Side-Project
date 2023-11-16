82552b4fcbd8c5c326f7023741efcaf3486e21e8
Ruben Ponce <rponce3@mail.sfsu.edu>
2023-11-16 13:42:47 -0800

    Update albi.js
    minor changes to help with styling

--- src/components/Teams/albi.js
+++ src/components/Teams/albi.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Container from 'react-bootstrap/esm/Container';
 
 import '../Styles/TeamPage.css';
+import '../Styles/IndivTeamPageStyle.css'
 import Albirex from "../images/Team_Logos/Albirex_Niigata.png"
 
 export default function Albi(){
@@ -15,26 +16,27 @@ export default function Albi(){
 
           <div className='title'> EST: </div> 
             <div className='value'>1955</div>
-          
+
           <div className='title'>Team names: </div>
             <div className='value'>
-              Niigata Eleven SC (1955)<br/>
-              Albireo Niigata FC (1995)<br/>
-              Albirex Niigata (1997) <br/>
+              Niigata Eleven SC (1955),
+              Albireo Niigata FC (1995),
+              Albirex Niigata (1997),
               Nickname: Albi
             </div> 
-
-          <div className='title'> Stadium: </div>
-            <div className='value'> 
-              Denka Big Swan Stadium<br/>
-              Capacity: 42,300<br/>
-              <i class="fas fa-map-marker-alt"> </i> Chuo-ku, Niigata<br/>
-            </div>
-
-          <div className='title'>Accomplishments: </div>
-            <div className='value'>
-              J2 Champion (2003, 2022)
-            </div>
+          
+            <div className='title'> Stadium: </div>
+              <div className='value'> 
+                Denka Big Swan Stadium<br/>
+                Capacity: 42,300<br/>
+                <i class="fas fa-map-marker-alt"> </i> Chuo-ku, Niigata<br/>
+              </div>
+              
+            <div className='title'>Accomplishments: </div>
+              <div className='value'>
+                J2 Champion (2003, 2022)
+              </div>
+          
           
         </div>
 
