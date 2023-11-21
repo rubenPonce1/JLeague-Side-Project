a5afe571e3211efe9624027783a9d93b7ca0c6f0
Ruben Ponce <rponce3@mail.sfsu.edu>
2023-11-21 14:30:08 -0800

    Update albi.js
    Added icons from FontAwsome as well as created the list of social media pages of Albi

--- src/components/Teams/albi.js
+++ src/components/Teams/albi.js
@@ -7,6 +7,7 @@ Description: This is the team page for Albirex Niigata
 
 import React from 'react';
 import Container from 'react-bootstrap/esm/Container';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 import '../Styles/TeamPage.css';
 import '../Styles/IndivTeamPageStyle.css'
@@ -77,14 +78,57 @@ export default function Albi(){
           </h2>
           <div className='social-media-area'>
             <ul className='social-media-list'>
-              <li className='social-media-item'>
-                <a className='social-media-value' href='https://www.albirex.co.jp/en/' target="_blank" rel="noreferrer"> 
-                  <img src={Albirex} alt = "logo"className ="social-media-img"/> 
+              {/*<li className='social-media-item'>
+                <a className='social-media-value' href='https://www.albirex.co.jp/en/' 
+                target="_blank" rel="noreferrer"> 
+                  <img src={Albirex} alt = "logo" className ="social-media-img"/> 
                   <div className='social-media-title'> Official Website </div>
                 </a> 
-              </li>
-              
+              </li> */}
 
+              <li className='social-media-item'>
+                <a className='social-media-value' href='https://twitter.com/albirex_pr?lang=en' 
+                target="_blank" rel="noreferrer">
+                  <div alt='albi_twt' className ="social-media-img"> 
+                  <FontAwesomeIcon icon="fab fa-twitter"/>
+                  </div>
+                  <div className='social-media-title'>@albirex_pr</div>
+                </a>
+              </li>
+
+              <li className='social-media-item'>
+                <a className='social-media-value' href='https://www.instagram.com/albirex_niigata_official/' 
+                target="_blank" rel="noreferrer">
+                  <div alt='albi_ig' className ="social-media-img">
+                    <FontAwesomeIcon icon="fab fa-instagram" />
+                  </div>
+                  <div className='social-media-title'> @albirex_niigata_official </div>
+                </a>
+              </li>
+
+              <li className='social-media-item'>
+                <a className='social-media-value' href='https://www.youtube.com/user/albirexniigata12' 
+                target="_blank" rel="noreferrer">
+                  <img alt='' className ="social-media-img" />
+                  <div className='social-media-title'> Official Youtube </div>
+                </a>
+              </li>
+
+              <li className='social-media-item'>
+                <a className='social-media-value' href='https://www.tiktok.com/@albirex_official' 
+                target="_blank" rel="noreferrer">
+                  <img alt='' className ="social-media-img" />
+                  <div className='social-media-title'> @albirex_official </div>
+                </a>
+              </li>
+
+              <li className='social-media-item'>
+                <a className='social-media-value' href='https://www.facebook.com/AlbirexNiigata' 
+                target="_blank" rel="noreferrer">
+                  <img alt='' className ="social-media-img" />
+                  <div className='social-media-title'> @アルビレックス新潟/Albirex Niigata </div>
+                </a>
+              </li>
             </ul>
 
           </div>
