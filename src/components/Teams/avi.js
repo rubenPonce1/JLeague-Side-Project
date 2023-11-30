1baac0a1e9d8108f93f1d90f80e6ff5045e20210
Ruben Ponce <rponce3@mail.sfsu.edu>
2023-11-30 13:22:15 -0800

    Update avi.js
    Added skeleton of the team page to avi.js:
    
    need to add changes to make it functional

---
+++ src/components/Teams/avi.js
@@ -0,0 +1,163 @@
+/*
+Filename: albi.js
+Date: 11/16/23
+Authors: Ruben Ponce
+Description: This is the team page for 
+*/
+
+
+import React from 'react';
+import Container from 'react-bootstrap/esm/Container';
+
+
+import '../Styles/TeamPage.css';
+import '../Styles/IndivTeamPageStyle.css'
+import from "../images/Team_Logos/.png"
+import { Link } from 'react-router-dom';
+
+
+export default function (){
+   return(
+     <div className=' '>
+       <h1 className='header'> <img src={} alt = "logo"className ="team-logo"></img>
+       
+       </h1>
+      
+       {/*This is the container for the team info */}
+       <div className='team-info'>
+         <div className='team-info-wrapper'>
+           <div className='info-title'> Info </div>
+
+
+           <div className='info-item'> EST: </div>
+             <div className='value'> *** </div>
+
+
+           <div className='info-item'>Team names: </div>
+             <div className='info-value'>
+               **** <br/>
+               Nickname: **
+             </div>
+          
+             <div className='info-item'> Stadium: </div>
+               <div className='info-value'>
+                ***<br/>
+                 Capacity: <br/>
+                 <i class="fas fa-map-marker-alt"> </i> * <br/>
+               </div>
+              
+             <div className='info-item'>Accomplishments: </div>
+               <div className='info-value'>
+                 *****
+               </div>
+        
+         </div>
+       </div>
+
+
+       <Container className='video-cntr'>
+         <h2 className='video-header'> Short Video of *****</h2>
+
+
+         <div class="ratio ratio-16x9">
+           <iframe src=" "
+                   title="YouTube video" allowFullScreen></iframe>
+         </div>
+
+
+         <div className='video-disc'>
+             Title of Channel:
+             <Link className='video-chnl-link' to={"  "}>
+               *** <br/>
+             </Link>
+             Title: *****
+            
+         </div>
+       </Container>
+
+
+	<footer className='social-media'>
+         <h2 className='social-media-hdr'>
+           <div className='social-media-hdr-title'>
+             *** Social Pages
+           </div>
+           <div class="social-media-hdr-title-underline" /> 
+         </h2>
+         <div className='social-media-area'>
+           <ul className='social-media-list'>
+            
+             <li className='social-media-item'>
+               <a className='social-media-value'
+               href=''
+               target="_blank" rel="noreferrer">
+                 <div>
+                 <FontAwesomeIcon  alt='***_ig'
+                 className ="social-media-img" icon="fab fa-twitter"/>
+                 </div>
+                 <div className='social-media-title'>@***</div>
+               </a>
+             </li>
+
+
+             <li className='social-media-item'>
+               <a className='social-media-value'
+               href=''
+               target="_blank" rel="noreferrer">
+                 <div>
+                   <FontAwesomeIcon  alt='***_ig'
+                   className ="social-media-img" icon="fab fa-instagram" />
+                 </div>
+                 <div className='social-media-title'> @*** </div>
+               </a>
+             </li>
+
+
+             <li className='social-media-item'>
+               <a className='social-media-value'
+               href=''
+               target="_blank" rel="noreferrer">
+                 <div>
+                   <FontAwesomeIcon  alt='***_ig'
+                   className ="social-media-img" icon="fab fa-youtube" />
+                 </div>
+                 <div className='social-media-title'> Official Youtube </div>
+               </a>
+             </li>
+
+
+             <li className='social-media-item'>
+               <a className='social-media-value'
+               href=''
+               target="_blank" rel="noreferrer">
+                 <div>
+                   <FontAwesomeIcon alt='***_ig'
+                   className ="social-media-img" icon="fab fa-tiktok" />
+                 </div>
+                 <div className='social-media-title'> @*** </div>
+               </a>
+             </li>
+
+
+             <li className='social-media-item'>
+               <a className='social-media-value'
+               href='https://www.facebook.com/AlbirexNiigata'
+               target="_blank" rel="noreferrer">
+                 <div>
+                   <FontAwesomeIcon alt='***_ig'
+                   className ="social-media-img" icon="fab fa-facebook-f" />
+                 </div>
+                 <div className='social-media-title'> @*** </div>
+               </a>
+             </li>
+           </ul>
+         </div>
+       </footer>
+      
+     </div>
+
+
+   );
+}  
+
+ 
+
