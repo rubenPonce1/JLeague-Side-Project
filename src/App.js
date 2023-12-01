af4f0d66bcf4c6f182134d608d527c3db0a7180d
Ruben Ponce <rponce3@mail.sfsu.edu>
2023-12-01 14:53:14 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,36 +5,35 @@ Authors: Ruben Ponce
 Description: Contains the Router for all components of the app
 */
 
-import React from 'react';
-import { Routes, Route, BrowserRouter  } from 'react-router-dom';
-import Navbar from './components/Navbar/Navbar';
-import Home from './components/Pages/Home';
-import Albi from './components/Teams/albi';
-import Avi from './components/Teams/avi'
-import Cerezo from './components/Teams/cerezo';
-import Hokkaido from './components/Teams/consa';
-import FCTokyo from './components/Teams/fctky';
-import Gamba from './components/Teams/gamba';
+import React from "react";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./components/Pages/Home";
+import Albi from "./components/Teams/albi";
+import Avi from "./components/Teams/avi";
+import Cerezo from "./components/Teams/cerezo";
+import Hokkaido from "./components/Teams/consa";
+import FCTokyo from "./components/Teams/fctky";
+import Gamba from "./components/Teams/gamba";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <React.Fragment>
-          <Navbar/>
+          <Navbar />
         </React.Fragment>
         <Routes>
-          <Route path = "/" element={<Home />} />
+          <Route path="/" element={<Home />} />
 
           {/*<Route path =  "*" element={<PageNotFound />} />*/}
 
-          <Route path = "/Albirex" element = {<Albi />} />
-          <Route path = "/Avispa" element= {<Avi />} />
-          <Route path = "/Cerezo" element= {<Cerezo />} />
-          <Route path = "/FC_Tokyo" element= {<FCTokyo />} />
-          <Route path = "/Gamba_Osaka" element= {<Gamba />} />
-          <Route path = "/Hokkaido" element= {<Hokkaido />} />
-
+          <Route path="/Albirex" element={<Albi />} />
+          <Route path="/Avispa" element={<Avi />} />
+          <Route path="/Cerezo" element={<Cerezo />} />
+          <Route path="/FC_Tokyo" element={<FCTokyo />} />
+          <Route path="/Gamba_Osaka" element={<Gamba />} />
+          <Route path="/Hokkaido" element={<Hokkaido />} />
         </Routes>
       </div>
     </BrowserRouter>
