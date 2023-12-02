5f13c8f3090f30a84d64cf7f02c740fc7dc9e201
Ruben Ponce <rponce3@mail.sfsu.edu>
2023-12-02 15:49:29 -0800

    Update reysol.js
    added video for Kashiwa reysol

--- src/components/Teams/reysol.js
+++ src/components/Teams/reysol.js
@@ -88,18 +88,25 @@ export default function Kashiwa() {
 
       {/*This is the container for the team video*/}
       <Container className="video-cntr">
-        <h2 className="video-header"> Short Video of *****</h2>
+        <h2 className="video-header"> Short Video of Kashiwa Reysol</h2>
 
         <div class="ratio ratio-16x9">
-          <iframe src=" " title="YouTube video" allowFullScreen></iframe>
+          <iframe
+            src="https://www.youtube.com/embed/eznc8Dnk5S8?si=gRCb2TsHHwqA7N56"
+            title="YouTube video"
+            allowFullScreen
+          ></iframe>
         </div>
 
         <div className="video-disc">
           Title of Channel:
-          <Link className="video-chnl-link" to={"  "}>
-            *** <br />
+          <Link
+            className="video-chnl-link"
+            to={"https://www.youtube.com/@JLEAGUEInternational"}
+          >
+            J.LEAGUE International <br />
           </Link>
-          Title: *****
+          Title: GET TO KNOW J.LEAGUE: Kashiwa Reysol
         </div>
       </Container>
 
