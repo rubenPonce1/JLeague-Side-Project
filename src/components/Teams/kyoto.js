0ae754ecf911919b8b785fc227da6ab8e622cd83
Ruben Ponce <rponce3@mail.sfsu.edu>
2023-12-02 19:33:50 -0800

    Update kyoto.js
    Added the kyoto video

--- src/components/Teams/kyoto.js
+++ src/components/Teams/kyoto.js
@@ -70,18 +70,25 @@ export default function Kyoto() {
 
       {/*This is the container for the team video*/}
       <Container className="video-cntr">
-        <h2 className="video-header"> Short Video of ***** </h2>
+        <h2 className="video-header"> Short Video of Kyoto Sanga </h2>
 
         <div class="ratio ratio-16x9">
-          <iframe src=" " title="YouTube video" allowFullScreen></iframe>
+          <iframe
+            src="https://www.youtube.com/embed/RU_k0oUATBI?si=OIXNm5ytTrkwsqzL"
+            title="YouTube video"
+            allowFullScreen
+          ></iframe>
         </div>
 
         <div className="video-disc">
           Title of Channel:
-          <Link className="video-chnl-link" to={"  "}>
-            *** <br />
+          <Link
+            className="video-chnl-link"
+            to={"https://www.youtube.com/@JLEAGUEInternational"}
+          >
+            J.LEAGUE International <br />
           </Link>
-          Title: *****
+          Title: GET TO KNOW J.LEAGUE: Kyoto Sanga
         </div>
       </Container>
 
