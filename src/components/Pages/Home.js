c0e510e5a354ffebb402ecc4cda5f89cc0a2fac1
Ruben Ponce <rponce3@mail.sfsu.edu>
2023-12-07 15:51:32 -0800

    Update Home.js

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -6,64 +6,66 @@ Description: File for the home page of the webpage
 it shows a Youtube video, has a image carousel, and team pages
 */
 
+import React from "react";
+import { TeamList } from "./TeamList";
+import { Link } from "react-router-dom";
+import "../Styles/Home.css";
 
-import React from 'react';
-import { TeamList } from './TeamList';
-import { Link } from 'react-router-dom';
-import '../Styles/Home.css';
+import "bootstrap/dist/css/bootstrap.css";
+import Container from "react-bootstrap/Container";
+import Carousel from "../Carousel/Carousel";
+import { homeSlides } from "../Carousel/ImgSlideshow";
 
-
-import 'bootstrap/dist/css/bootstrap.css';
-import Container from 'react-bootstrap/Container';
-import Carousel from '../Carousel/Carousel';
-import { homeSlides } from '../Carousel/ImgSlideshow';
-
-
-
-export default function Home(){
-  return(
+export default function Home() {
+  return (
     <div>
-      <div className='home'>
-        <h1 className='HeaderName'> J.LEAGUE </h1>
+      <div className="home">
+        <h1 className="HeaderName"> J.LEAGUE </h1>
 
         {/*This Container is for the Youtube Video */}
-        <Container className='video-cntr'>
+        <Container className="video-cntr">
           <div class="ratio ratio-16x9">
-            <iframe src="https://www.youtube.com/embed/Mu3H6-SbGGQ" 
-                    title="YouTube video" allowFullScreen></iframe>
-          </div>  
+            <iframe
+              src="https://www.youtube.com/embed/Mu3H6-SbGGQ"
+              title="YouTube video"
+              allowFullScreen
+            ></iframe>
+          </div>
         </Container>
 
-
         {/* This is an image slideshow */}
-        <div className='home-carousel'>
-          <Carousel images={homeSlides}/>
+        <div className="home-carousel">
+          <Carousel images={homeSlides} />
         </div>
-        
 
-        
         {/* This is the list of all the J1 Teams*/}
-        <div className='team-list-cntr'>
-        <h2 className='team-list-hdr'> TEAM LIST</h2>
-          <ul className='team-list'>
+        <div className="team-list-cntr">
+          <h2 className="team-list-hdr"> TEAM LIST</h2>
+          <ul className="team-list">
             {TeamList.map((item, index) => {
-              return(
-                <li key={TeamList.index} >
-                  <Link className={item.cName}
-                        to = {item.path} target="_blank" rel="noreferrer">
-
-                      <img src= {item.image} className= "team-logo" alt='teamLogo'></img>
-                      {item.title}  
-                      
-                  </Link>   
+              return (
+                <div className="item">
+                <li key={TeamList.index}>
+                  <Link
+                    className={item.cName}
+                    to={item.path}
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <img
+                      src={item.image}
+                      className={item.logoName}
+                      alt="team-logo"
+                    ></img>
+                    {item.title}
+                  </Link>
                 </li>
-              )
+                </div>
+              );
             })}
           </ul>
         </div>
-      
+      </div>
     </div>
-    </div>
-    );
-       
+  );
 }
